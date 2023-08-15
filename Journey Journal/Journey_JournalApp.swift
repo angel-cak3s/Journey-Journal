0b310c89e8f04418a340a24f649fd32804c039fa
Angela <angelatran.tn@gmail.com>
2023-08-15 09:59:54 -0700

    Initial Commit

---
+++ Journey Journal/Journey_JournalApp.swift
@@ -0,0 +1,17 @@
+//
+//  Journey_JournalApp.swift
+//  Journey Journal
+//
+//  Created by scholar on 8/15/23.
+//
+
+import SwiftUI
+
+@main
+struct Journey_JournalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
