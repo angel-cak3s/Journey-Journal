a5f25410110e6dd0a99a679e82f0cd41b8fb5571
Angela <angelatran.tn@gmail.com>
2023-08-17 10:16:28 -0700

    Journal draft 2

--- Journey Journal/Journals.swift
+++ Journey Journal/Journals.swift
@@ -9,43 +9,84 @@ import SwiftUI
 
 struct Journals: View {
     var body: some View {
-        ZStack {
-            // first layer = painting
-            Color(red: 1, green: 0.98, blue: 0.96)
-                .ignoresSafeArea()
-            VStack {
-                Image("Journals")
-                    .resizable()
-                    .aspectRatio(contentMode: .fill)
-                    .frame(width: 326, height: 109)
-                    .clipped()
-                    .padding()
-            
+        
+        NavigationStack {
+            ZStack {
+                // first layer = painting
+                Color(red: 1, green: 0.98, blue: 0.96)
+                    .ignoresSafeArea()
                 
+                VStack {
+                    Image("Journals")
+                        .resizable()
+                        .aspectRatio(contentMode: .fill)
+                        .frame(width: 326, height: 109)
+                        .clipped()
+                        .padding()
+                    
+                    
+                    HStack {
+                        
+                        NavigationLink(destination: InJournal()) {
+                            Image("Green_Book")
+                                .resizable()
+                                .aspectRatio(contentMode: .fill)
+                                .frame(width: 100, height: 138)
+                                .clipped()                                        }
+
+                        Image("Pink_Book")
+                            .resizable()
+                            .aspectRatio(contentMode: .fill)
+                            .frame(width: 100, height: 138)
+                            .clipped()
+                        
+                        Image("Blue_Book")
+                            .resizable()
+                            .aspectRatio(contentMode: .fill)
+                            .frame(width: 100, height: 138)
+                            .clipped()
+                    }
+                    
+                    .padding()
+                    
                 HStack {
-                    RoundedRectangle(cornerRadius: 10)
-                        .frame(width: 100, height: 129)
                     
-                    RoundedRectangle(cornerRadius: 10)
-                        .frame(width: 100, height: 129)
+                    Image("Yellow_Book")
+                            .resizable()
+                            .aspectRatio(contentMode: .fill)
+                            .frame(width: 100, height: 138)
+                            .clipped()
+                        
+                        Image("Purple_Book")
+                            .resizable()
+                            .aspectRatio(contentMode: .fill)
+                            .frame(width: 100, height: 138)
+                            .clipped()
+                        
+                        Image("Orange_Book")
+                            .resizable()
+                            .aspectRatio(contentMode: .fill)
+                            .frame(width: 100, height: 138)
+                            .clipped()
+                    }
                     
-                    RoundedRectangle(cornerRadius: 10)
-                        .frame(width: 100, height: 129)
+                    
+                    Spacer()
+
+                    
+                    
                 }
                 
                 
-                Spacer()
-                
-            
             }
-            
-            
         }
+
     }
     
-    struct Journals_Previews: PreviewProvider {
+    
+    struct ContentView_Previews: PreviewProvider {
         static var previews: some View {
-            Journals()
+            ContentView()
         }
     }
 }
