00569cee5babf8be37fd94bc2cdf0260efcc5a6d
Mina Gwak <minagwak921@gmail.com>
2023-08-16 20:08:57 -0700

    Made changes to sketchbooks

--- Journey Journal/Scrapbooks.swift
+++ Journey Journal/Scrapbooks.swift
@@ -1,5 +1,5 @@
 //
-//  Scrapbooks.swift
+//  Journals.swift
 //  Journey Journal
 //
 //  Created by scholar on 8/16/23.
@@ -9,12 +9,66 @@ import SwiftUI
 
 struct Scrapbooks: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            // first layer = painting
+            Color(red: 1, green: 0.98, blue: 0.96)
+                .ignoresSafeArea()
+            VStack {
+                Image("Scrapbooks")
+                    .resizable()
+                    .aspectRatio(contentMode: .fill)
+                    .frame(width: 350, height: 100)
+                    .clipped()
+
+                
+                Image("Scrapbook Paper")
+                    .resizable()
+                    .aspectRatio(contentMode: .fill)
+                    .clipped()
+                    .padding()
+                
+            struct Stickers: View {
+                    
+                    @State private var dragAmount = CGSize.zero
+                    
+                    var name: String
+                    var index: Int
+                    
+                    var body: some View {
+                        
+                        Image(systemName: name)
+                            .resizable()
+                            .frame(width: 80, height: 80)
+                            .offset(dragAmount)
+                            .zIndex(dragAmount == .zero ? 0 : 1)
+                            .gesture(
+                                DragGesture(coordinateSpace: .global)
+                                    .onChanged {
+                                        self.dragAmount = CGSize(width: $0.translation.width, height: $0.translation)
+                                    }
+                            
+                            )
+                    }
+                }
+                
+                
+            
+                
+                
+                
+                Spacer()
+                
+            
+            }
+            
+            
+        }
+    }
+    
+    struct Scrapbooks_Previews: PreviewProvider {
+        static var previews: some View {
+            Scrapbooks()
+        }
     }
 }
 
-struct Scrapbooks_Previews: PreviewProvider {
-    static var previews: some View {
-        Scrapbooks()
-    }
-}
