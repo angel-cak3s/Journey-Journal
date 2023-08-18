6b5c592bda49e385fd287152c177db2820fb9308
Mina Gwak <minagwak921@gmail.com>
2023-08-18 08:32:14 -0700

    made info page

--- Journey Journal/Info.swift
+++ Journey Journal/Info.swift
@@ -19,7 +19,29 @@ struct Info: View {
                     .font(.largeTitle)
                     .fontWeight(.semibold)
                 
-                Text("How Art can improve your Mental Health")
+                Text("How Art can improve your Mental Health \n")
+                
+                Text("Stress Relief")
+                    .fontWeight(.semibold)
+                
+                Text ("Studies have shown that drawing, doodling, coloring, and simply creating something for 20+ minutes reduces cortisol, which helps regulate your body’s response to stress. \n")
+                
+                     
+                Text("Boosts Self Esteem")
+                    .fontWeight(.semibold)
+                      
+                Text ("Creativity and the practice of art can build a sense of accomplishment for all ages, as it 'makes us feel good about ourselves' and validates our  our ability to turn unique ideas into something physical \n")
+                      
+                Text ("Healing")
+                    .fontWeight(.semibold)
+                
+                Text ("It has been proven by much research that art making can help in processing pain, and provide a direct connection between to mind and the body to further aid to healing process \n")
+                      
+                Text ("Letting Go")
+                    .fontWeight(.semibold)
+                
+                Text ("Expressive art, such as visual arts, music, and dance, can help users laugh, let go, and relax, thus decreasing depression, anxiety, and stress.")
+                    
 
             }
         }
